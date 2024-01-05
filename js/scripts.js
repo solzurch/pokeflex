2b4fbf85f28ee4263f7aa3c5fd7dd6aab9470a35
solzurch <151952240+solzurch@users.noreply.github.com>
2024-01-06 00:39:59 +0100

    update javaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,45 @@
-alert('Hello world');
+// height in m, weight in kg
 
-let favoriteFood = 'Burgers';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: '0.7',
+        types: ['grass', 'poison'],
+        weight: '6.9'
+    },
+
+    {
+        name: 'Charmaleon',
+        height: '1.1',
+        types: 'fire',
+        weight: '19'
+    },
+
+    {
+        name: 'Blastoise',
+        height: '1.6',
+        types: 'water',
+        weight: '85.5'
+    },
+
+    {
+        name: 'Weedle',
+        height: '0.3',
+        types: ['bug', 'poison'],
+        weight: '3.2'
+    },
+
+    {
+        name: 'Spearow',
+        height: '0.3',
+        types: ['flying', 'normal'],
+        weight: '2'
+    },
+
+    {
+        name: 'Pikachu',
+        height: '0.4',
+        types: electric,
+        weight: '6'
+    }
+];
