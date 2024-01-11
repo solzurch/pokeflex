0f38c452093ec973205f292761d8bf55cd5525e3
solzurch <151952240+solzurch@users.noreply.github.com>
2024-01-11 20:19:54 +0100

    create IIFE
    wrap the pokemonList array in an IIFE and create a new pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,45 @@
 // height in m, weight in kg
 
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: '0.7',
-        types: ['grass', 'poison'],
-        weight: '6.9'
-    },
-
-    {
-        name: 'Charmaleon',
-        height: '1.1',
-        types: 'fire',
-        weight: '19'
-    },
-
-    {
-        name: 'Blastoise',
-        height: '1.6',
-        types: 'water',
-        weight: '85.5'
-    },
-];
-
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: '0.7',
+            types: ['grass', 'poison'],
+            weight: '6.9'
+        },
+        {
+            name: 'Charmaleon',
+            height: '1.1',
+            types: 'fire',
+            weight: '19'
+        },
+        {
+            name: 'Blastoise',
+            height: '1.6',
+            types: 'water',
+            weight: '85.5'
+        },
+    ];
+    return {
+        add: function (pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function () {
+            return pokemonList;
+        }
+    };
+})();
 
 // this is old (for loop)
 // for (let i = 0; i < pokemonList.length; i++) {
 //     let pokemonInfo = pokemonList[i].name + " (height: " + pokemonList[i].height + ")";
 
-pokemonList.forEach(function(pokemon) {
-  document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")" + "</p>");
+pokemonRepository.getAll().forEach(function (pokemon) {
+    document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")" + "</p>");
 
-if (pokemon.height >1.5) {
-    document.write(" -Wow, that's big!!")
-    //Hi Blaise! how can I put the info next to it?
-}
+    if (pokemon.height > 1.5) {
+        document.write(" -Wow, that's big!!")
+        //Hi Blaise! how can I put the info next to it?
+    }
 })
